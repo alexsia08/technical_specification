745112429cc8ba79bfa64bae1cd661ca1f6c08cf
alexsia08 <nastutulka@gmail.com>
2025-06-20 12:02:47 +0700

    task

--- src/widgets/HomePage/Library/constants/styles.ts
+++ src/widgets/HomePage/Library/constants/styles.ts
@@ -49,7 +49,7 @@ export const CSSButton = styled.button(function () {
 export const CSSCards = styled.div(function () {
   return {
     width: "150px",
-    height: "150px",
+    height: "100px",
     borderRadius: "10px",
     padding: "10px",
     display: "flex",
